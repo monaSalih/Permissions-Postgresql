97c4ac3c9e5d6989b1357f9424e1d2b083c017f9
monaSalih <monaalobisat@gmail.com>
2021-11-22 18:31:41 +0200

    solve lab and add test

--- snacks/views.py
+++ snacks/views.py
@@ -1,11 +1,15 @@
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from .models import Snack
 from .serializers import SnackSerlizer
+from .permissions import IsAuthorOrReadOnly
 
 class SnackList(ListCreateAPIView):
+    permission_classes = (IsAuthenticatedOrReadOnly,)
     queryset = Snack.objects.all()
     serializer_class = SnackSerlizer
 
 class SnackDetail(RetrieveUpdateDestroyAPIView):
+    permission_classes = (IsAuthorOrReadOnly,)
     queryset = Snack.objects.all()
     serializer_class = SnackSerlizer
